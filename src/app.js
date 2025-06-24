f0fca6e087d361a5a6167343543d0cfeeaff1c22
trivmvirat <consvpr@outlook.es>
2025-06-24 23:07:21 +0000

    dominios

--- src/app.js
+++ src/app.js
@@ -5,23 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   //  
 };
 
- let pronoun = ['the', 'our'];
-  let adj = ['great', 'big'];
-  let noun = ['jogger', 'racoon'];
+let final = [".com", ".net", ".us", ".io"];
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
 
 
 
-  pronoun.map((pro)=>{
-    adj.map((adjetivo)=>{
-     noun.map((nombrePropio)=>{
-       console.log(pro+ " "+adjetivo+ " "+nombrePropio);
-     }
-    )
-    })
-  })
+final.map((terminacion) => {
+  pronoun.map((pro) => {
+    adj.map((adjetivo) => {
+      noun.map((nombrePropio) => {
+        console.log(pro + " " + adjetivo + " " + nombrePropio + terminacion);
+     });
+    });
+  });
+});
 
